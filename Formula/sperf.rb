53474ba48c2d6fd8caefa30da3f2ee6ec12b2481
Ryan Svihla <rs@foundev.pro>
2020-07-17 21:08:23 +0200

    hash mismatch

--- Formula/sperf.rb
+++ Formula/sperf.rb
@@ -4,7 +4,7 @@ class Sperf < Formula
   desc "Datastax Enterprise and Apache Cassandra diagnostic tooling from DataStax"
   homepage "https://github.com/DataStax-Toolkit/sperf"
   url "https://github.com/DataStax-Toolkit/sperf/archive/v0.6.3.tar.gz"
-  sha256 "393e7ad462b0857d5bfb1d65d06944f5971a6d4dcfed9e1ee04a6196b6bf0b85"
+  sha256 "2009c9c3df3a70308f6e2c26c87787c582b7e6f72028d6bb024acf7473e269a1"
   license "Apache-2.0"
   depends_on "python@3.8"
 
